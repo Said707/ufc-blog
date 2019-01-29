858a8c4f991ac8feb5875f17a39821b4847ad196
Said <sayid707@mail.ru>
2019-01-30 01:25:07 +0500

    UFC Blog

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView)
+from . import views 
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<slug>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<slug>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+]
+
+
